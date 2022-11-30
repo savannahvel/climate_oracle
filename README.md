924442481ebfd4464dbee8fab4c88832b9f17a17
Savannah Vel <savannah.vel@vivint.com>
2022-11-30 09:37:06 -0800

    finishing touches, working project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Image by <a href="https://www.freepik.com/free-photo/beautiful-skyscape-during-daytime_22542932.htm#query=weather&position=49&from_view=search&track=sph">Freepik</a>
+# Climate Oracle
+
+## Description
+
+Check the weather by entering a zip code and get a 5 day forecast. 
+
+![alt text](./assests/images/screencapture-127-0-0-1-5502-index-html-2022-10-30-14_41_49.png)
+
+## Usage
+Deployed code at [Climate Oracle](https://savannahvel.github.io/climate_oracle/)
+
+## Credits
+
+Background:
+<a href="https://www.freepik.com/free-photo/beautiful-skyscape-during-daytime_22542932.htm#query=weather&position=49&from_view=search&track=sph">Image by Freepik</a>
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,8 +20,7 @@ function recentSearch() {
         zipCode = lastSearch;
         getLatAndLon();
     } else {
-        zipCode = 97035;
-        getLatAndLon();
+        return
     }
 }
 
@@ -44,29 +43,57 @@ function getLatAndLon() {
         .then((data) => {
             let lat = data.city.coord.lat;
             let lon = data.city.coord.lon;
-            
-            setHero(data)
-            getWeather(lat, lon)
+            let city = data.city.name.toUpperCase();
+            // setHero(data)
+            getWeather(lat, lon, city);
     })
 }
 
-function setHero(data) {
-    console.log(data);
+function setHero(city, data) {
+    // console.log(data);
     // clearing instructions if app has been used previously
     let instructions = document.querySelector('#instructions');
     instructions.innerHTML = '';
-    let displayLocation = document.querySelector('#city');
+
     // displayLocation
+    let displayLocation = document.querySelector('#city');
+    displayLocation.innerHTML = city;
+
+    // display weather
+    let current_weather = document.querySelector('#current-weather');
+    current_weather.innerHTML = data.daily
+        .map((day, idx) => {
+            console.log(day)
+            if (idx <= 0) {
+                var date = dayjs.unix(day.dt).format('ddd');
+                let weatherType = day.weather[0].description;
+                let tempHigh = day.temp.max;
+                let tempLow = day.temp.min;
+                let humidity = day.humidity;
+                let wind = day.wind_speed;
+                let sunrise = dayjs.unix(day.sunrise).format('h:mm a');
+                let sunset = dayjs.unix(day.sunset).format('h:mm a');
+
+
+                return `<section class="p-2 m-1">
+                    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" ">
+                    <div class="card-body">
+                        <h4 class="card-title">${weatherType}</h4>
+                        <p class="card-text">High Temp <b>${tempHigh}</b> Low Temp <b>${tempLow}</b> Humidity <b>${humidity}</b> Wind Speed <b>${wind}</b> Sunrise <b>${sunrise}</b> Sunset <b>${sunset}</b></p>
+                    </div>
+                </section>`
+            }
+    }).join(' ');
 }
 
 
-function getWeather(lat, lon) {
+function getWeather(lat, lon, city) {
     let weatherApi = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${api_key}`
 
     fetch(weatherApi)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            setHero(city, data)
             fiveDayForecast(data)
     })
 }
@@ -84,10 +111,12 @@ function fiveDayForecast(resp) {
                 let weatherType = day.weather[0].description;
                 let tempHigh = day.temp.max;
                 let tempLow = day.temp.min;
+                let humidity = day.humidity;
+                let wind = day.wind_speed;
                 let sunrise = dayjs.unix(day.sunrise).format('h:mm a');
                 let sunset = dayjs.unix(day.sunset).format('h:mm a');
                 
-                return `<section class="p-4 m-1 border border-primary rounded bg-info"><b>${date}</b> <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"> <b>${weatherType}</b> High Temp <b>${tempHigh}</b> Low Temp <b>${tempLow}</b> Sunrise <b>${sunrise}</b> Sunset <b>${sunset}</b></section>`
+                return `<section class="p-4 m-1 border border-primary rounded bg-info"><b>${date}</b> <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"> <b>${weatherType}</b> High Temp <b>${tempHigh}</b> Low Temp <b>${tempLow}</b> Humidity <b>${humidity}</b> Wind Speed <b>${wind}</b> Sunrise <b>${sunrise}</b> Sunset <b>${sunset}</b></section>`
             }
     }).join(' ');
 }
